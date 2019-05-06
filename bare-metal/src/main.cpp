8e0663c48da1f3d77d615e74146436c41e299c64
caiopo <caiopoliveira@gmail.com>
2019-05-06 02:21:11 -0300

    Add bss tests and cleanup assembly

--- bare-metal/src/main.cpp
+++ bare-metal/src/main.cpp
@@ -30,11 +30,29 @@ SideEffect se_global3("global 3");
 SideEffect se_global4("global 4");
 SideEffect se_global5("global 5");
 
+int zero_array[] = {0, 0, 0, 0, 0, 0};
+
 extern "C" {
     int main() {
         SideEffect se_local("local");
 
+        bool is_zero = true;
+
+        for (int i = 0; i < sizeof(zero_array) / sizeof(zero_array[0]); ++i) {
+            if (zero_array[i] != 0) {
+                is_zero = false;
+            }
+        }
+
+        if (is_zero) {
+            print_uart0("zero_array was initialized\n");
+        } else {
+            print_uart0("zero_array was NOT initialized!!!\n");
+        }
+
         print_uart0("Hello world!\n");
+
+        return 0;
     }
 
     void atexit() {}
--- bare-metal/src/startup.s
+++ bare-metal/src/startup.s
@@ -1,17 +1,17 @@
 .section .vector_table
-.global _Reset
-_Reset:
-b Reset_Handler
-b . // 0x4 Undefined Instruction
-b . // 0x8 Software Interrupt
-b . // 0xC Prefetch Abort
-b . // 0x10 Data Abort
-b . // 0x14 Reserved
-b . // 0x18 IRQ
-b . // 0x1C FIQ
+.global _reset
+_reset:
+b _start // 0x0 Reset
+b .      // 0x4 Undefined Instruction
+b .      // 0x8 Software Interrupt
+b .      // 0xC Prefetch Abort
+b .      // 0x10 Data Abort
+b .      // 0x14 Reserved
+b .      // 0x18 IRQ
+b .      // 0x1C FIQ
 
 .section .entry
-Reset_Handler:
+_start:
 // init stack
 ldr sp, =_stack_end
 
@@ -21,31 +21,32 @@ ldr r1, =_bss_start
 ldr r2, =_bss_end
 
 bss_loop:
-cmp r1, r2
+cmp   r1, r2
 strlt r0, [r1], #4
-blt bss_loop
+blt   bss_loop
 
 // init static objects
 ldr r0, =_init_array_start
 ldr r1, =_init_array_end
 
 globals_init_loop:
-cmp     r0, r1
-it      lt
-ldrlt   r2, [r0], #4
-blxlt   r2
-blt     globals_init_loop
+cmp   r0, r1
+ldrlt r2, [r0], #4
+blxlt r2
+blt   globals_init_loop
 
+// jump to main
 bl main
 
+// destroy static objects in reverse order
 ldr r0, =_fini_array_start
 ldr r1, =_fini_array_end
 
 globals_fini_loop:
-cmp     r0, r1
-it      lt
-ldrlt   r2, [r0], #4
-blxlt   r2
-blt     globals_fini_loop
+cmp   r0, r1
+ldrlt r2, [r0], #4
+blxlt r2
+blt   globals_fini_loop
 
+// freeze forever
 b .
